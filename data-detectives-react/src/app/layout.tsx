db10b2be30667c603761df60bf96a79b27365400
rocio <rocioyatzky@gmail.com>
2023-06-26 23:30:01 -0300

    cambios mp

--- data-detectives-react/src/app/layout.tsx
+++ data-detectives-react/src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
           useRefreshTokens={true}
           useRefreshTokensFallback={false}
           authorizationParams={{
-            redirect_uri: "http://health-time-git-promocion-data-detectives-react.vercel.app"+router,
+            redirect_uri: "http://localhost:3000"+router,
             audience: 'https://data-detectives-laravel.vercel.app/rest',
             scope: 'openid profile email'
           }}
--- data-detectives-react/src/app/mercadoPago/page.tsx
+++ data-detectives-react/src/app/mercadoPago/page.tsx
@@ -1,22 +1,27 @@
 "use client";
 import { CardPayment, initMercadoPago } from '@mercadopago/sdk-react';
 import ContainerPaciente from "../paciente/containerPaciente";
+import { useAuth0 } from '@auth0/auth0-react';
 
 interface MercadoPagoPageProps {
   onPaymentComplete: (response: any) => void;
 }
 
 const MercadoPagoPage: React.FC<MercadoPagoPageProps> = ({ onPaymentComplete }) => {
+
+  const { getAccessTokenSilently } = useAuth0();
  
   initMercadoPago("TEST-749a0c68-d11d-474a-a390-888b31a17b66");
 
   const onSubmit = async (formData: any) => {
     // callback llamado al hacer clic en el botón enviar datos
+    const token = await getAccessTokenSilently(); 
     return new Promise<void>((resolve, reject) => {
       fetch('https://data-detectives-laravel-git-promo-data-detectives.vercel.app/rest/process_payment', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
         },
         body: JSON.stringify(formData),
       })
